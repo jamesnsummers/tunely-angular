cf87e9d85ad7d82fb5c4e08ed0589edd1e1cef6e
James Summers <jamesnsummers@gmail.com>
2017-06-05 12:31:54 -0500

    completed sprint 1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,33 @@
 /* CLIENT-SIDE JS
  *
- * This is your main angular file. Edit as you see fit.
+ * This is the main angular file.
  *
  */
+
+angular
+.module('tunely', [])
+.controller('AlbumsIndexController', AlbumsIndexController);
+
+function AlbumsIndexController () {
+  var vm = this;
+  vm.newAlbum = {};
+
+  vm.albums = [
+  {
+  name: 'License to Ill',
+  artistName: 'Beastie Boys'
+  },
+  {
+  name: 'Coming Home',
+  artistName: 'Leon Bridges'
+  },
+  {
+  name: 'Are We There',
+  artistName: 'Sharon Von Etten'
+  },
+  {
+  name: 'The Queen is Dead',
+  artistName: 'The Smiths'
+  }
+];
+}
