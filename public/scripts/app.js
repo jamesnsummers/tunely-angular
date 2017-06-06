043e36cbbc74e5d6ad5608d2115892295756ffe0
James Summers <jamesnsummers@gmail.com>
2017-06-06 11:56:06 -0500

    made POST function work and cleaned up styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,12 +13,6 @@ angular
 AlbumsIndexController.$inject = ['$http'];
 function AlbumsIndexController ($http) {
   var vm = this;
-  vm.newAlbum = {};
-
-  vm.newAlbum = {
-      name: 'Viva Hate',
-      artistName: 'Morrissey'
-  };
 
   $http({
     method: 'GET',
@@ -28,4 +22,16 @@ function AlbumsIndexController ($http) {
   }, function errorCallback(response) {
     console.log('There was an error getting the data', response);
   });
+
+  vm.createAlbum = function () {
+    $http({
+      method: 'POST',
+      url: '/api/albums',
+      data: vm.newAlbum,
+    }).then(function successCallback(response) {
+      vm.albums.push(response.data);
+    }, function errorCallback(response) {
+      console.log('There was an error posting the data', response);
+    });
+  }
 }
