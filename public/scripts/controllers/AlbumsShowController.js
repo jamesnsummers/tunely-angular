4ce5f29e7d2364c28d1ecc903ee5633f93dfc6bc
James Summers <jamesnsummers@gmail.com>
2017-06-08 09:25:02 -0500

    adding edit and delete buttons to albumsShow page

--- public/scripts/controllers/AlbumsShowController.js
+++ public/scripts/controllers/AlbumsShowController.js
@@ -17,4 +17,16 @@ function AlbumsShowController ($http, $routeParams) {
     console.log('There was an error getting the data', response);
   });
 
+  vm.editAlbum = function (album) {
+    $http({
+      method: 'PUT',
+      url: '/api/albums/'+album._id,
+      data: album
+    }).then(function successCallback(json) {
+      // don't need to do anything!
+    }, function errorCallback(response) {
+      console.log('There was an error editing the data', response);
+    });
+  }
+
 }
